cc62c5f14211a19b1e85a30a51d4fb7335767d9c
Jackson Coelho <fcjack@gmail.com>
2016-02-28 18:03:11 -0300

    Revert "Commit inicial."
    This reverts commit ad44004c2434a2625841603f223094912e0b160f.

--- README.md
+++
@@ -1,3 +0,0 @@
-# programacao-para-web
-Trabalho do curso de especialização em Arquitetura, Design e Implementação e Sistemas para Internet;
-Disciplina de Programação para Web;
--- src/main/java/br/edu/fa7/cursojsf/repository/CidadeRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.fa7.cursojsf.repository;
-
-import br.edu.fa7.cursojsf.model.Cidade;
-
-public class CidadeRepository extends AbstractCrudRepository<Cidade> {
-
-}
