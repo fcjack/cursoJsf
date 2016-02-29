42d8fbf2202391d8ea489c00464e381a6314804e
Jackson Coelho <fcjack@gmail.com>
2016-02-29 10:10:22 -0300

    Mudança no script de dados iniciais

--- src/main/resources/initial_data.sql
+++ src/main/resources/initial_data.sql
@@ -1,8 +1,6 @@
 INSERT INTO user (id, username, password) VALUES (1, 'admin', 'admin');
 
 INSERT INTO estado (id, nome, sigla) VALUES (1, 'Ceará', 'CE');
-INSERT INTO estado (id, nome, sigla) VALUES (2, 'Rio de Janeiro', 'RJ');
-INSERT INTO estado (id, nome, sigla) VALUES (3, 'São Paulo', 'SP');
 
 INSERT INTO cidade (id, nome, dataContituicao, populacao, pib, estado_id) VALUES (1, 'Fortaleza', '1726-04-13', 2591188, 37106309000, 1);
 INSERT INTO cidade (id, nome, dataContituicao, populacao, pib, estado_id) VALUES (2, 'Caucaia', '1939-01-01', 325441, 4557457, 1);
